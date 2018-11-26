539722b89dd9a1cf6536558e79f09151c2391c82
ivankp <ivan.pogrebnyak@gmail.com>
2018-11-26 03:41:05 -0500

    init

---
+++ src/table.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <array>
+
+#include "ivanp/io/mem_file.hh"
+#include "ivanp/scribe.hh"
+#include "ivanp/math/vec4.hh"
+
+#define TEST(var) \
+  std::cout << "\033[36m" #var "\033[0m = " << var << std::endl;
+
+using std::cout;
+using std::endl;
+
+using namespace ivanp;
+
+int main(int argc, char* argv[]) {
+  if (argc!=3 && argc!=2) {
+    cout << "usage: " << argv[0] << " file.dat[.xz]\n";
+    return 1;
+  }
+
+  const mem_file file = (
+    ends_with(argv[1],".xz") || ends_with(argv[1],".lzma")
+    ? mem_file::pipe(cat("unxz -c ",argv[1]).c_str())
+    : mem_file::mmap(argv[1])
+  );
+
+  scribe::reader sr(file.mem(),file.size());
+  // const auto& head = sr.head();
+  // TEST(head);
+
+  cout << "[["
+    "\"runNumber\",\"eventNumber\","
+    "\"γγ pT [GeV]\",\"γγ m [GeV]\",\"pT γ1/pT γ2\""
+  "]";
+
+  const auto event_t = sr.get_type()[0][0];
+  const auto photons_i = event_t.index("photons");
+  const auto runNumber_i = event_t.index("runNumber");
+  const auto eventNumber_i = event_t.index("eventNumber");
+
+  for (auto event : sr[0]) {
+    const auto& photons = event[photons_i].cast<float(&)[2][4]>();
+    using vec = vec4<double>;
+    const std::array<vec,2> y {{
+      { photons[0], vec::PtEtaPhiM },
+      { photons[1], vec::PtEtaPhiM }
+    }};
+    const auto yy = y[0] + y[1];
+    const auto pT_yy = yy.pt();
+    const auto m_yy = yy.m();
+
+    if (pT_yy < 250) continue;
+    if (m_yy < 121 || 129 < m_yy) continue;
+
+    cout << ",\n["
+      << event[runNumber_i].cast<uint32_t>() << ','
+      << event[eventNumber_i].cast<uint64_t>() << ','
+      << yy.pt() << ','
+      << yy.m() << ','
+      << (y[0].pt()/y[1].pt()) << ']';
+  }
+  cout << "]";
+}
