db122df6328ed49ca92b568309908195a527f8fc
ivankp <ivan.pogrebnyak@gmail.com>
2018-11-28 15:43:42 -0500

    nmax, more vars

--- src/explorer.cc
+++ src/explorer.cc
@@ -58,7 +58,7 @@ public:
 
 struct event {
   vec4<> y[2];
-  std::vector<vec4<>> jets;
+  std::vector<vec4<>> j;
 } e;
 
 int main(int argc, char* argv[]) {
@@ -66,17 +66,26 @@ int main(int argc, char* argv[]) {
     cerr << "usage: " << argv[0] << " file.dat\n";
     return 1;
   }
+  const unsigned nmax = 1000;
 
   struct fcn_t {
     double(*f)();
     bool need_jets;
   };
   std::map<const char*,fcn_t,less_str> fcns {
-    { "pT_y1", { []{ return e.y[0].pt(); }, false } },
-    { "pT_y2", { []{ return e.y[1].pt(); }, false } },
     { "pT_yy", { []{ return (e.y[0]+e.y[1]).pt(); }, false } },
     { "m_yy",  { []{ return (e.y[0]+e.y[1]).m(); }, false } },
-    { "rat_pT_y1_y2", { []{ return e.y[0].pt()/e.y[1].pt(); }, false } }
+    { "pT_y1", { []{ return e.y[0].pt(); }, false } },
+    { "pT_y2", { []{ return e.y[1].pt(); }, false } },
+    { "rat_pT_y1_y2", { []{ return e.y[0].pt()/e.y[1].pt(); }, false } },
+    { "eta_y1", { []{ return e.y[0].eta(); }, false } },
+    { "eta_y2", { []{ return e.y[1].eta(); }, false } },
+
+    { "Njets", { []{ return (double)e.j.size(); }, true } },
+    { "pT_j1", { []{ return e.j[0].pt(); }, true } },
+    { "pT_j2", { []{ return e.j[1].pt(); }, true } },
+    { "eta_j1", { []{ return e.j[0].eta(); }, true } },
+    { "eta_j2", { []{ return e.j[1].eta(); }, true } }
   };
 
   json req;
@@ -95,8 +104,8 @@ int main(int argc, char* argv[]) {
   }
 
   bool need_jets = false;
-  const auto& req_cuts = req.at("cuts");
   std::vector<std::function<bool()>> cuts;
+  const auto& req_cuts = req["cuts"];
   cuts.reserve(req_cuts.size());
   for (const auto& cut : req_cuts) {
     const auto& fcn = fcns.at(cut.at(0).get_ref<const std::string&>().c_str());
@@ -118,6 +127,7 @@ int main(int argc, char* argv[]) {
     if (fcn.need_jets) need_jets = true;
   }
 
+  unsigned nselected = 0;
   uint32_t runNumber;
   uint64_t eventNumber;
   uint32_t njets;
@@ -133,10 +143,10 @@ int main(int argc, char* argv[]) {
     e.y[1] = { mom, vec4<>::PtEtaPhiM };
     dat >> njets;
     if (need_jets) {
-      e.jets.resize(njets);
+      e.j.resize(njets);
       for (decltype(njets) i=0; i<njets; ++i) {
         dat >> mom;
-        e.jets[i] = { mom, vec4<>::PtEtaPhiM };
+        e.j[i] = { mom, vec4<>::PtEtaPhiM };
       }
     } else {
       dat.skip(sizeof(mom)*njets);
@@ -145,14 +155,17 @@ int main(int argc, char* argv[]) {
     for (const auto& cut : cuts)
       if (!cut()) goto next_event;
 
-    if (first) first = false;
-    else cout << ',';
-    cout << '[' << runNumber << ',' << eventNumber;
-    for (const auto& var : vars)
-      cout << ',' << var();
-    cout << "]";
+    if (++nselected <= nmax) {
+      if (first) first = false;
+      else cout << ',';
+      cout << '[' << runNumber << ',' << eventNumber;
+      for (const auto& var : vars)
+        cout << ',' << var();
+      cout << ']';
+    }
 
 next_event: ;
   }
+  if (nselected > nmax) cout << ',' << (nselected-nmax);
   cout << "]";
 }
